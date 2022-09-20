6bc92a7c257c7ee62ba6f5094ba9dbb58ca3646b
Yi_Zhang <zhangyi970819@gmail.com>
2022-09-20 17:27:48 +0800

    Update training_train.sh

--- JDet/training_train.sh
+++ JDet/training_train.sh
@@ -1,1 +1,1 @@
-use_parallel_op_compiler=8 python /project/train/src_repo/OCR-Baseline-main/JDet/tools/run_net.py --config-file=/project/train/src_repo/OCR-Baseline-main/JDet/store_sign_detection/s2anet_r50_fpn_5x_ocr_630_1120_bs4.py --task=train
+HIP_VISIBLE_DEVICES=0 use_parallel_op_compiler=4 python /project/train/src_repo/OCR-Baseline-main/JDet/tools/run_net.py --config-file=/project/train/src_repo/OCR-Baseline-main/JDet/store_sign_detection/s2anet_r50_fpn_5x_ocr_630_1120_bs4.py --task=train
