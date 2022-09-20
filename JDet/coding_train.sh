1bdc04a61fe9e5cdd7ec772a4ee8f4487ae3bf33
Yi_Zhang <zhangyi970819@gmail.com>
2022-09-20 17:28:02 +0800

    Update coding_train.sh

--- JDet/coding_train.sh
+++ JDet/coding_train.sh
@@ -1,1 +1,1 @@
-use_parallel_op_compiler=8 python tools/run_net.py --config-file=store_sign_detection/s2anet_r50_fpn_5x_ocr_630_1120_bs4.py --task=train
+HIP_VISIBLE_DEVICES=0 use_parallel_op_compiler=4 python tools/run_net.py --config-file=store_sign_detection/s2anet_r50_fpn_5x_ocr_630_1120_bs4.py --task=train
